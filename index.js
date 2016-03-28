e0cec1928265b16bf9cbf8d434d0220d390275c3
BinRui Guan <binrui.guan@hirede.com>
2016-03-28 21:51:25 +0800

    Inital Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var nodeSass = require('node-sass');
+var rollupPluginutils = require('rollup-pluginutils');
+
+function plugin() {
+    var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+    var filter = rollupPluginutils.createFilter(options.include || [ '**/*.sass', '**/*.scss' ], options.exclude || 'node_modules/**');
+
+    return {
+        transform: function transform(code, id) {
+            if (!filter(id)) {
+                return null;
+            }
+
+            return new Promise(function (resolve, reject) {
+                nodeSass.render(Object.assign({
+                    data: code
+                }, options.config || {}), function (error, result) {
+                    var temp = {
+                        code: result,
+                        map: { mappings: '' }
+                    };
+
+                    if (error) {
+                        temp.error = error;
+                        reject(error);
+                    }
+
+                    resolve(temp);
+                });
+            });
+        }
+    };
+}
+
+plugin.compiler = compiler;
+module.exports = plugin;
