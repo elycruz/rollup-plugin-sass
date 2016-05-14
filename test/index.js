aa458e1f84e31919151595434475479d18243f5d
BinRui Guan <binrui.guan@hirede.com>
2016-05-14 16:07:31 +0800

    Add test

---
+++ test/index.js
@@ -0,0 +1,76 @@
+import test from 'ava';
+import { readFileSync } from 'fs';
+import { rollup } from 'rollup';
+import sass from '..';
+
+const sassOptions = {
+    outputStyle: 'compressed'
+};
+
+test('should import *.scss and *.sass files', t => {
+    return rollup({
+        entry: 'samples/basic/index.js',
+        plugins: [
+            sass({
+                options: sassOptions
+            })
+        ]
+    }).then(bundle => {
+        const code = bundle.generate().code;
+        const style1 = readFileSync('samples/basic/style1.scss').toString();
+        const style2 = readFileSync('samples/basic/style2.sass').toString();
+
+        t.true(code.indexOf(style1) > -1);
+        t.true(code.indexOf(style2) > -1);
+    });
+});
+
+test('should process code with output function', t => {
+    let outputCode = '';
+    let timestamp = String(Date.now());
+
+    return rollup({
+        entry: 'samples/output-function/index.js',
+        plugins: [
+            sass({
+                output(code) {
+                    return outputCode = code + timestamp;
+                },
+                options: sassOptions
+            })
+        ]
+    }).then(bundle => {
+        const code = bundle.generate().code;
+        const style = readFileSync('samples/output-function/style.scss').toString();
+
+        t.truthy(outputCode);
+        t.is(outputCode, `${style}\n${timestamp}`);
+    })
+});
+
+test('should process support promise', t => {
+    let outputCode;
+    let timestamp = String(Date.now());
+
+    return rollup({
+        entry: 'samples/output-promise/index.js',
+        plugins: [
+            sass({
+                output(code) {
+                    return new Promise((resolve) => {
+                        setTimeout(() => {
+                            resolve(outputCode = code + timestamp)
+                        }, 100);
+                    });
+                },
+                options: sassOptions
+            })
+        ]
+    }).then(bundle => {
+        const code = bundle.generate().code;
+        const style = readFileSync('samples/output-promise/style.scss').toString();
+
+        t.truthy(outputCode);
+        t.is(outputCode, `${style}\n${timestamp}`);
+    });
+});
---
+++ test/samples/basic/index.js
@@ -0,0 +1,4 @@
+import style1 from './style1.scss';
+import style2 from './style2.sass';
+
+export default style1 + style2;
