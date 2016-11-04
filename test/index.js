ca7dee52ab050074281f8b624b9bfa2f7e501340
BinRui Guan <binrui.guan@hirede.com>
2016-11-04 16:08:43 +0800

    Fix test case for windows

--- test/index.js
+++ test/index.js
@@ -8,6 +8,10 @@ const sassOptions = {
     outputStyle: 'compressed'
 };
 
+function squareWhitespace(str) {
+    return str.trim().replace(/\r/, '');
+}
+
 test('should import *.scss and *.sass files', t => {
     return rollup({
         entry: 'fixtures/basic/index.js',
@@ -17,12 +21,12 @@ test('should import *.scss and *.sass files', t => {
             })
         ]
     }).then(bundle => {
-        const code = bundle.generate().code.trim();
+        const code = squareWhitespace(bundle.generate().code);
         const style1 = readFileSync('fixtures/basic/style1.scss').toString();
         const style2 = readFileSync('fixtures/basic/style2.sass').toString();
 
-        t.true(code.indexOf(style1.trim()) > -1);
-        t.true(code.indexOf(style2.trim()) > -1);
+        t.true(code.indexOf(squareWhitespace(style1)) > -1);
+        t.true(code.indexOf(squareWhitespace(style2)) > -1);
     });
 });
 
@@ -44,7 +48,7 @@ test('should process code with processor', t => {
         const style = readFileSync('fixtures/processor/style.scss').toString();
 
         t.truthy(outputCode);
-        t.is(outputCode.trim(), `${style.trim()}`);
+        t.is(squareWhitespace(outputCode), `${squareWhitespace(style)}`);
     })
 });
 
@@ -70,7 +74,7 @@ test('should processor support promise', t => {
         const style = readFileSync('fixtures/processor-promise/style.scss').toString();
 
         t.truthy(outputCode);
-        t.is(outputCode.trim(), `${style.trim()}`);
+        t.is(squareWhitespace(outputCode), `${squareWhitespace(style)}`);
     });
 });
 
@@ -93,8 +97,8 @@ test('should support output as (non-previously existent)-path', t => {
         const output = readFileSync(fullfile).toString();
 
         removeSync(testPath);
-        t.is(code.trim(), '');
-        t.is(output.trim(), `${style1}${style2}`.trim());
+        t.is(squareWhitespace(code), '');
+        t.is(squareWhitespace(output), squareWhitespace(`${style1}${style2}`));
     });
 });
 
@@ -116,8 +120,8 @@ test('should support output as function', t => {
         const style1 = readFileSync('fixtures/output-function/style1.scss').toString();
         const style2 = readFileSync('fixtures/output-function/style2.scss').toString();
 
-        t.is(code.trim(), '');
-        t.is(outputCode.trim(), `${style1}${style2}`.trim());
+        t.is(squareWhitespace(code), '');
+        t.is(squareWhitespace(outputCode), squareWhitespace(`${style1}${style2}`));
     });
 });
 
@@ -139,8 +143,8 @@ test('should support output as true', t => {
         const style2 = readFileSync('fixtures/output-true/style2.scss').toString();
         const output = readFileSync('fixtures/output-true/output.css').toString();
 
-        t.is(code.trim(), '');
-        t.is(output.trim(), `${style1}${style2}`.trim());
+        t.is(squareWhitespace(code), '');
+        t.is(squareWhitespace(output), squareWhitespace(`${style1}${style2}`));
     });
 });
 
@@ -167,9 +171,9 @@ test('should insert CSS into head tag', t => {
                     t.true(mockNode.hasOwnProperty('setAttribute'));
 
                     if (count === 0) {
-                        t.is(mockNode.innerHTML.trim(), `${style1}`.trim());
+                        t.is(squareWhitespace(mockNode.innerHTML), squareWhitespace(`${style1}`));
                     } else if (count === 1) {
-                        t.is(mockNode.innerHTML.trim(), `${style2}`.trim());
+                        t.is(squareWhitespace(mockNode.innerHTML), squareWhitespace(`${style2}`));
                     }
 
                     count++;
