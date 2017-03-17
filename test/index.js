5898789c21f23529de249b016c332a6c20bc7a01
BinRui Guan <binrui.guan@gmail.com>
2017-03-17 08:31:53 +0800

    Fix typo

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ const sassOptions = {
   outputStyle: 'compressed'
 }
 
-function squareWhitespace (str) {
+function squash(str) {
   return str.trim().replace(/\r/, '')
 }
 
@@ -21,12 +21,12 @@ test('should import *.scss and *.sass files', t => {
       })
     ]
   }).then(bundle => {
-    const code = squareWhitespace(bundle.generate().code)
+    const code = squash(bundle.generate().code)
     const style1 = readFileSync('fixtures/basic/style1.scss').toString()
     const style2 = readFileSync('fixtures/basic/style2.sass').toString()
 
-    t.true(code.indexOf(squareWhitespace(style1)) > -1)
-    t.true(code.indexOf(squareWhitespace(style2)) > -1)
+    t.true(code.indexOf(squash(style1)) > -1)
+    t.true(code.indexOf(squash(style2)) > -1)
   })
 })
 
@@ -47,7 +47,7 @@ test('should process code with processor', t => {
     const style = readFileSync('fixtures/processor/style.scss').toString()
 
     t.truthy(outputCode)
-    t.is(squareWhitespace(outputCode), `${squareWhitespace(style)}`)
+    t.is(squash(outputCode), `${squash(style)}`)
   })
 })
 
@@ -72,7 +72,7 @@ test('should processor support promise', t => {
     const style = readFileSync('fixtures/processor-promise/style.scss').toString()
 
     t.truthy(outputCode)
-    t.is(squareWhitespace(outputCode), `${squareWhitespace(style)}`)
+    t.is(squash(outputCode), `${squash(style)}`)
   })
 })
 
@@ -95,8 +95,8 @@ test('should support output as (non-previously existent)-path', t => {
     const output = readFileSync(fullfile).toString()
 
     removeSync(testPath)
-    t.is(squareWhitespace(code), '')
-    t.is(squareWhitespace(output), squareWhitespace(`${style1}${style2}`))
+    t.is(squash(code), '')
+    t.is(squash(output), squash(`${style1}${style2}`))
   })
 })
 
@@ -118,8 +118,8 @@ test('should support output as function', t => {
     const style1 = readFileSync('fixtures/output-function/style1.scss').toString()
     const style2 = readFileSync('fixtures/output-function/style2.scss').toString()
 
-    t.is(squareWhitespace(code), '')
-    t.is(squareWhitespace(outputCode), squareWhitespace(`${style1}${style2}`))
+    t.is(squash(code), '')
+    t.is(squash(outputCode), squash(`${style1}${style2}`))
   })
 })
 
@@ -141,8 +141,8 @@ test('should support output as true', t => {
     const style2 = readFileSync('fixtures/output-true/style2.scss').toString()
     const output = readFileSync('fixtures/output-true/output.css').toString()
 
-    t.is(squareWhitespace(code), '')
-    t.is(squareWhitespace(output), squareWhitespace(`${style1}${style2}`))
+    t.is(squash(code), '')
+    t.is(squash(output), squash(`${style1}${style2}`))
   })
 })
 
@@ -169,9 +169,9 @@ test('should insert CSS into head tag', t => {
           t.true(mockNode.hasOwnProperty('setAttribute'))
 
           if (count === 0) {
-            t.is(squareWhitespace(mockNode.innerHTML), squareWhitespace(`${style1}`))
+            t.is(squash(mockNode.innerHTML), squash(`${style1}`))
           } else if (count === 1) {
-            t.is(squareWhitespace(mockNode.innerHTML), squareWhitespace(`${style2}`))
+            t.is(squash(mockNode.innerHTML), squash(`${style2}`))
           }
 
           count++
