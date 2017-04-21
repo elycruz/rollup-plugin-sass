156fe3fcaa9d2c56cf925646e48affa3b5071fec
differui <differui@gmail.com>
2017-04-21 16:45:40 +0800

    Update deps

--- rollup.config.js
+++ rollup.config.js
@@ -5,19 +5,20 @@ export default {
   format: 'cjs',
   dest: 'dist/rollup-plugin-sass.js',
   external: [
+    'babel-runtime/regenerator',
     'babel-runtime/core-js/json/stringify',
     'babel-runtime/core-js/object/assign',
     'babel-runtime/helpers/asyncToGenerator',
-    'babel-runtime/regenerator',
-    'fs',
-    'util',
     'path',
+    'fs',
     'node-sass',
+    'util',
     'rollup-pluginutils',
     'fs-extra'
   ],
   plugins: [
     babel({
+      exclude: './node_modules/**',
       runtimeHelpers: true
     })
   ]
