bbe0fa7f2b78eac65de561f4648f027f14c214d6
differui <differui@gmail.com>
2017-06-16 17:11:32 +0800

    Update test cases

--- test/fixtures/processor/index.js
+++ test/fixtures/processor/index.js
@@ -1,3 +1,4 @@
-import style from './style.scss'
+import atualA from '../../assets/actual_a.scss'
+import atualB from '../../assets/actual_b.scss'
 
-export default style
+export default atualA + atualB
---
+++ test/fixtures/typescript/index.ts
@@ -0,0 +1,11 @@
+import atualA from '../../assets/actual_a.scss'
+import atualB from '../../assets/actual_b.scss'
+
+// typescript code snippet
+function identity(arg: number): number {
+    return arg;
+}
+
+identity(5)
+
+export default actualA + actualB
