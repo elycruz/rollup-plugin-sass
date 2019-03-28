fe850a085e13a97fb0cc87ac92bc568cfcd5fbd8
differui <differui@gmail.com>
2019-03-28 18:53:55 +0800

    Update test entrance related with package.json

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs';
 import { removeSync } from 'fs-extra';
 import { rollup } from 'rollup';
 import sassJs from 'sass';
-import sass from '../dist/index';
+import sass from '..';
 
 const sassOptions = {
   outputStyle: 'compressed',
