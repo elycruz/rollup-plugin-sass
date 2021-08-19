1011b1158b00234a016b7f2f53c86025c8308ce2
elycruz <603428+elycruz@users.noreply.github.com>
2021-08-19 16:01:07 -0400

    #78 - Downgraded typescript version.  Rebuilt artifacts.

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -1,6 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isFunction = exports.isObject = exports.isString = exports.isset = exports.error = exports.warn = exports.log = void 0;
-const isset = x => x !== null && x !== undefined, isString = x => exports.isset(x) && x.constructor === String, isObject = x => typeof x === 'object', isFunction = x => typeof x === 'function';
-exports.log = console.log.bind(console), exports.warn = console.warn.bind(console), exports.error = console.error.bind(console), exports.isset = isset, exports.isString = isString, exports.isObject = isObject, exports.isFunction = isFunction;
+exports.log = console.log.bind(console), exports.warn = console.warn.bind(console), exports.error = console.error.bind(console), exports.isset = x => x !== null && x !== undefined, exports.isString = x => exports.isset(x) && x.constructor === String, exports.isObject = x => typeof x === 'object', exports.isFunction = x => typeof x === 'function';
 //# sourceMappingURL=index.js.map
