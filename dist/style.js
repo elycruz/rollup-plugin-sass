bd77caddcfae52f883cea14a9ed346183f1db50c
Ely De La Cruz <ely.delacruz@publicissapient.com>
2021-08-19 16:09:57 -0400

    Revert "#78 - Downgraded typescript version.  Rebuilt artifacts."
    This reverts commit 1011b1158b00234a016b7f2f53c86025c8308ce2.

--- dist/style.js
+++ dist/style.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.insertStyle = void 0;
 function insertStyle(css) {
     if (!css || !window) {
         return;
